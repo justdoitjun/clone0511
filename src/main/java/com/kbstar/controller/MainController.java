3cb88c54b1063c50c15852f62586206b91661f7d
junstdoitjun <bing74700@gmail.com>
2023-05-11 17:44:32 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.kbstar.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class MainController {
+    @RequestMapping("/")
+    public String main(Model model){
+        return "index";
+    }
+}
